f612bdb71a9363c0cbbb2b9f3aef2a906059c525
vahabvahabzada <vahabvahabzad@gmail.com>
2023-06-01 15:56:20 +0400

    Added user-search functionality

--- src/main/java/com/vlachat/app/services/SearchService.java
+++ src/main/java/com/vlachat/app/services/SearchService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import com.vlachat.app.daos.SearchDao;
 import com.vlachat.app.dtos.UserDto;
 import com.vlachat.app.entities.User;
+import com.vlachat.app.exceptions.CannotBeEmptyException;
 import com.vlachat.app.mappers.UserMapper;
 
 public class SearchService {
@@ -15,6 +16,9 @@ public class SearchService {
     }
 
     public List<UserDto> searchUser(String searchText){
+        if(searchText==null){
+            throw new CannotBeEmptyException("Search text can not be null!");
+        }
         UserMapper mapper=new UserMapper();
         List<User> users=searchDao.searchUser(searchText);
         List<UserDto> results=new LinkedList<>();
